95e2e24fcbc2466efaf2850edfb1270642dcbe6e
Samikshya <write2samikshyakar@gmail.com>
2021-10-11 14:16:42 +0530

    Add project files.

---
+++ WebApplication1_Learning1_/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1_Learning1_.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>ListBox</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="ListBox1" runat="server" SelectionMode="Multiple">
+            <asp:ListItem Text="Doctor" Value="1"></asp:ListItem>
+            <asp:ListItem Text="Engineer" Value="2"></asp:ListItem>
+            <asp:ListItem Text="Writer" Value="3"></asp:ListItem>
+            <asp:ListItem Text="Teacher" Value="4"></asp:ListItem>                
+            </asp:ListBox>
+            <br />            <br />
+
+            
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+            <br />
+            <br />
+                    <asp:Button ID="Button2" runat="server" Text="Button" OnClick="Button2_Click" />
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ WebApplication1_Learning1_/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1_Learning1_
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(ListBox1.SelectedItem !=null)
+            {
+                Response.Write("Text = " + ListBox1.SelectedItem.Text + "<br/>");
+                Response.Write("Value = " + ListBox1.SelectedItem.Value + "<br/>");
+                Response.Write("Index = " + ListBox1.SelectedIndex.ToString());
+
+            }
+            else
+            {
+                Response.Write("Please select an item");
+
+            }
+            
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            foreach(ListItem li in ListBox1.Items)
+            {
+                if(li.Selected)
+                {
+                    Response.Write(" Text = " + li.Text + "<br/>");
+                    Response.Write("Value = " + li.Value + "<br/>");
+                    Response.Write("Index = " + ListBox1.Items.IndexOf(li).ToString() +"<br/>");
+                    Response.Write("<br/>"+ "________________________");
+                    Response.Write("<br/>");
+
+                }
+               
+            }
+
+           
+        }
+    }
+}
