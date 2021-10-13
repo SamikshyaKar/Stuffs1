edc8ec29cb2048c945d27395458fafe3e43f94c8
Samikshya <write2samikshyakar@gmail.com>
2021-10-13 15:12:30 +0530

    next commit1

---
+++ WebApplication1_Learning1_/WebForm13.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm13.aspx.cs" Inherits="WebApplication1_Learning1_.WebForm13" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:HiddenField ID="HiddenField1" runat="server" />
+            <table>
+                <tr>
+               <td><b>Name:</b></td>
+                    <td>
+                        
+                    <asp:TextBox ID="TxtName" runat="server"></asp:TextBox>
+                    </td>
+
+                </tr>
+
+                 <tr>
+               <td>Gender:</td>
+                    <td>
+
+                        
+                    <asp:TextBox ID="TxtGender" runat="server"></asp:TextBox>
+                    </td>
+
+                </tr>
+                 <tr>
+               <td>Department</td>
+                    <td>
+                    <asp:TextBox ID="TxtDept" runat="server"></asp:TextBox>
+
+                    </td>
+
+                </tr>
+
+            </table>
+            <asp:Button ID="UploadEmp"  runat="server" Text="Upload Emp" OnClick="Button1_Click" Width="108px" /> 
+
+            
+            <asp:Button ID="UpdateEmp"  runat="server" Text="Update Emp" OnClick="Button2_Click" style="margin-left: 23px" Width="133px" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1_Learning1_/WebForm13.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication1_Learning1_
+{
+    public partial class WebForm13 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                loadEmployee();
+            }
+        }
+
+        private void loadEmployee()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using(SqlConnection Con= new SqlConnection(CS))
+            {
+                string sqlQuery = "Select Id, Name, Gender, DeptName from tblEmployeesforFramework where Id=202";
+                SqlCommand cmd = new SqlCommand(sqlQuery, Con);
+                Con.Open();
+                using(SqlDataReader rdr= cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        TxtName.Text = rdr["Name"].ToString();
+                        TxtGender.Text = rdr["Gender"].ToString();
+                        TxtDept.Text = rdr["DeptName"].ToString();
+                        HiddenField1.Value = rdr["Id"].ToString();
+                    }
+                }
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            loadEmployee();
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection  Con = new SqlConnection(CS))
+            {
+                string SqlQuery = "update tblEmployeesforFramework set Name=@Name, Gender=@Gender,DeptName=@DeptName" +
+                    " where Id=@Id";
+                SqlCommand cmd = new SqlCommand(SqlQuery, Con);
+
+                cmd.Parameters.AddWithValue("@Name", TxtName.Text);
+                cmd.Parameters.AddWithValue("@Gender", TxtGender.Text);
+                cmd.Parameters.AddWithValue("@DeptName", TxtDept.Text);
+                cmd.Parameters.AddWithValue("@Id", HiddenField1.Value);
+                Con.Open();
+                cmd.ExecuteNonQuery();
+                Con.Close();
+
+                TxtName.Text = " ";
+                TxtGender.Text = " ";
+                TxtDept.Text = " ";                
+            }
+        }
+    }
+}
